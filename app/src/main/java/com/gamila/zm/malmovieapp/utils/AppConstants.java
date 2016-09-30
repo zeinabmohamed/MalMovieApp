201254dc75f8a323441c9cb68e1b9ba4676c4324
zeinabmohamed <zeinabmohamed22@gmail.com>
2016-09-30 21:32:03 +0200

    adding picasso lib and image util

---
+++ app/src/main/java/com/gamila/zm/malmovieapp/utils/AppConstants.java
@@ -0,0 +1,30 @@
+package com.gamila.zm.malmovieapp.utils;
+
+import android.support.annotation.StringDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by Zeinab Mohamed on 9/30/2016.
+ */
+
+public class AppConstants {
+
+    public static String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+
+    @StringDef({IMAGE_SIZE.W_92,
+            IMAGE_SIZE.W_154, IMAGE_SIZE.W_185, IMAGE_SIZE.W_342, IMAGE_SIZE.W_500, IMAGE_SIZE.W_780})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface IMAGE_SIZE {
+
+        String W_92 = "w92";
+        String W_154 = "w154";
+        String W_185 = "w185";
+        String W_342 = "w342";
+        String W_500 = "w500";
+        String W_780 = "w780";
+    }
+
+
+}
---
+++ app/src/main/java/com/gamila/zm/malmovieapp/utils/ImageUtil.java
@@ -0,0 +1,38 @@
+package com.gamila.zm.malmovieapp.utils;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by Zeinab Mohamed on 9/30/2016.
+ */
+
+public class ImageUtil {
+
+
+    private static ImageUtil imageUtil;
+
+    public static ImageUtil getInstance() {
+        if (imageUtil == null) {
+            imageUtil = new ImageUtil();
+        }
+        return imageUtil;
+    }
+
+    private ImageUtil() {
+        //no instance
+    }
+
+    public void loadImageByUrlInImageView(Context context, String imageStringUrl, ImageView imageView) {
+        Picasso.with(context).load(imageStringUrl).into(imageView);
+    }
+
+    public void loadImageByNameInImageView(Context context, String imageName, ImageView imageView) {
+        Picasso.with(context).load(AppConstants.IMAGE_BASE_URL + AppConstants.IMAGE_SIZE.W_185 +
+                "/" + imageName).into(imageView);
+    }
+
+
+}
