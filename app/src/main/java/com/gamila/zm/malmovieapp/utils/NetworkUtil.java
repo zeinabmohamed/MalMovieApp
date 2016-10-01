e9b3d8d0956f2e2bb660c26f316e1444ec0b5d99
zeinabmohamed <zeinabmohamed22@gmail.com>
2016-10-01 02:50:19 +0200

    adding network util

---
+++ app/src/main/java/com/gamila/zm/malmovieapp/utils/NetworkUtil.java
@@ -0,0 +1,20 @@
+package com.gamila.zm.malmovieapp.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by Zeinab Mohamed on 10/1/2016.
+ */
+
+public class NetworkUtil {
+
+    public boolean isOnline(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+}
